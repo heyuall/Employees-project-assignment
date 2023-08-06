b484ac71e4f2730bb5a12156dd48d54739bc280b
heyuall <zoubeidihoussem@gmail.com>
2023-08-06 21:53:58 +0100

    First commit

---
+++ src/main/java/com/johns_group_hr_system/project_employees_assignments/EmployeesAssignmentsApplication.java
@@ -0,0 +1,13 @@
+package com.johns_group_hr_system.project_employees_assignments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesAssignmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesAssignmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/johns_group_hr_system/project_employees_assignments/EmployeesAssignmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johns_group_hr_system.project_employees_assignments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesAssignmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
