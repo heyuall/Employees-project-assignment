eca3ccba7cc4039bd1aacd1c7cd3e64e26cdeb03
heyuall <zoubeidihoussem@gmail.com>
2023-08-26 18:49:23 +0100

    Fix filter

--- src/main/java/com/johns_group_hr_system/project_employees_assignments/repository/IEmployeeRepository.java
+++ src/main/java/com/johns_group_hr_system/project_employees_assignments/repository/IEmployeeRepository.java
@@ -1,14 +1,16 @@
 package com.johns_group_hr_system.project_employees_assignments.repository;
 
 import com.johns_group_hr_system.project_employees_assignments.entity.Employee;
+import com.johns_group_hr_system.project_employees_assignments.entity.enums.EmployeeRoleEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface IEmployeeRepository extends JpaRepository<Employee, UUID> {
-    List<Employee> findByNameContainingIgnoreCaseAndRoleIgnoreCaseAndProjectsId(String partialName, String role, UUID projectId);
+    List<Employee> findByNameContainingIgnoreCaseAndRoleIgnoreCaseAndProjectsId(@NotNull String name, @NotNull EmployeeRoleEnum role, UUID projects_id);
 
 }
