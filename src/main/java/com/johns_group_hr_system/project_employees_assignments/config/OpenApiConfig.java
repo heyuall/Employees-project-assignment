d0f7def624dcd3bd1113bd4833c9bdbec632e068
heyuall <zoubeidihoussem@gmail.com>
2023-08-27 15:17:38 +0100

    Add swagger ui apis documentation

---
+++ src/main/java/com/johns_group_hr_system/project_employees_assignments/config/OpenApiConfig.java
@@ -0,0 +1,40 @@
+package com.johns_group_hr_system.project_employees_assignments.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI usersMicroserviceOpenAPI() {
+//        return new OpenAPI()
+//                .info(new Info().title("Employee-Assignments-Projects")
+//                        .description("End points available")
+//                        .version("1.0"));
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info().title("My REST API")
+                        .description("Some custom description of API.")
+                        .version("1.0").contact(new Contact().name("Houssem")
+                                .email("houssem@gmail.com").url("www.Houssem.com"))
+                        .license(new License().name("License of API")
+                                .url("API license URL")));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+}
